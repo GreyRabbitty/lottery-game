69446fe22b59acd318cd13ce41bfb1549d234018
Grey Rabbitty <GreyRabbitty@gmail.com>
2023-07-04 15:33:25 +0700

    re-design players table

--- client/src/components/Players.jsx
+++ client/src/components/Players.jsx
@@ -20,11 +20,11 @@ export default function Players({ accounts, players, balance, web3 }) {
         </tbody>
       </table>
       <p className="mt-2 mb-1">List of current players' address:</p>
-      <ul className="list-group">
+      {/* <ul className="list-group">
         {players.map((player) => (
           <li className="list-group-item" key={player}>
             <span className="fs-6 fst-italic">
-              <span className="badge text-bg-warning">{player["number"]}</span>{" "}
+              </span>{" "}
               {player["playerAddress"]}{" "}
               {accounts[0] === player["playerAddress"] && (
                 <span className="fw-semibold text-danger">(me)</span>
@@ -32,7 +32,29 @@ export default function Players({ accounts, players, balance, web3 }) {
             </span>
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th className="text-center">No.</th>
+            <th className="text-center">Address</th>
+            <th className="text-center">Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players?.map((player, index) => (
+            <tr>
+              <td className="text-center">{index + 1}</td>
+              <td className="text-center">
+                <span className="fs-6 fst-italic">{player?.playerAddress}</span>
+              </td>
+              <td className="text-center">
+                <span className="badge text-bg-warning">{player?.number}</span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
